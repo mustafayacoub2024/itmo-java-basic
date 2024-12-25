b6c405f7774a3dce65e1522a5869221b30f9df74
mustafayacoub2024 <mustafa.yacoub2013@mail.ru>
2024-12-25 21:38:34 +0300

    Laboratory work №4

--- src/lab4/Part1/Task2.java
+++ src/lab4/Part1/Task2.java
@@ -17,13 +17,13 @@ public class Task2 {
         System.out.println();
 
         for (int i = 1; i <= num; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 3 == 0 & i % 5 == 0) {
                 System.out.print(i + ",");
             }
         }
     }
 
     public static void main(String[] args) {
-        result(100);
+        result(10);
     }
 }
--- src/lab4/Part1/Task4.java
+++ src/lab4/Part1/Task4.java
@@ -2,7 +2,7 @@ package lab4.Part1;
 
 public class Task4 {
     public static boolean result(int num1, int num2, int num3){
-        return num1<=num2 && num2<=num3;
+        return num1<=num2 & num2<=num3;
     }
     public static void main(String[] args) {
         System.out.println(result(5,10,15));
