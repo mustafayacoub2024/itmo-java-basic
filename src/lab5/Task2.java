03d79c5dde0dcb8a2c449a012bd1e893e7daf833
mustafayacoub2024 <mustafa.yacoub2013@mail.ru>
2024-12-27 22:33:30 +0300

    Update Task2.java

--- src/lab5/Task2.java
+++ src/lab5/Task2.java
@@ -1,14 +1,14 @@
 package lab5;
 
 public class Task2 {
-    public static boolean result(String str) {
+    public static boolean  isPalindrome(String str) {
         String str1 = str.toLowerCase();
         String revers = new StringBuilder(str1).reverse().toString();
         return (str1.equals(revers));
     }
 
     public static void main(String[] args) {
-        System.out.println(result("Казак"));
-        System.out.println(result("Маша"));
+        System.out.println( isPalindrome("Казак"));
+        System.out.println( isPalindrome("Маша"));
     }
 }
