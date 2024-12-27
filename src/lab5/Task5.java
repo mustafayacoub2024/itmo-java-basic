95d405231ad07af5aaad195c1b16d1b097be8b72
mustafayacoub2024 <mustafa.yacoub2013@mail.ru>
2024-12-27 22:35:31 +0300

    Update Task5.java

--- src/lab5/Task5.java
+++ src/lab5/Task5.java
@@ -1,7 +1,7 @@
 package lab5;
 
 public class Task5 {
-    public static void result(String str) {
+    public static void invertWordsInAString(String str) {
 
         for (String word : str.split(" ")) {
             StringBuilder builder = new StringBuilder(word);
@@ -11,6 +11,6 @@ public class Task5 {
     }
 
     public static void main(String[] args) {
-        result("This is a test string");
+        invertWordsInAString("This is a test string");
     }
 }
